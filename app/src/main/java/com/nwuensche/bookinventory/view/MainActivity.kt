ad09cdd8e628edfa1c7362d1a6de6101063a21db
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2018-01-26 14:44:58 +0100

    Add Adapter in MainActivity

--- app/src/main/java/com/nwuensche/bookinventory/view/MainActivity.kt
+++ app/src/main/java/com/nwuensche/bookinventory/view/MainActivity.kt
@@ -3,11 +3,16 @@ package com.nwuensche.bookinventory.view
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.nwuensche.bookinventory.R
+import com.nwuensche.bookinventory.model.DataStorage
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val arrayAdapter = BookAdapter(this, DataStorage.text)
+        bookList.adapter = arrayAdapter
     }
 }
